ab99a605580c8ed1aecd1380dcbb3d3d136be231
Aman Singh <aman.singh@qsstechnosoft.com>
2026-02-26 13:34:33 +0530

    add Logo of association

--- sections/association.php
+++ sections/association.php
@@ -1,80 +1,28 @@
-<section id="association" class="py-5 bg-white">
-  <div class="container text-center">
-    <h2 class="association-title mb-2">Our <span class="text-magenta">Association</span></h2>
-    <div class="association-underline mb-5"></div>
+<section id="association" class="association-section">
+  <div class="container">
+    <div class="association-header">
+      <h2>Our <span>Association</span></h2>
+      <p>We collaborate with trusted technology and infrastructure partners to deliver reliable solar solutions.</p>
+    </div>
 
-    <div id="associationCarousel" class="carousel slide" data-bs-ride="carousel">
-      <!-- Controls -->
-      <div class="testimonial-controls mb-4">
-        <button class="btn testimonial-btn me-2" type="button" data-bs-target="#associationCarousel" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon-custom" aria-hidden="true">&lsaquo;</span>
-        </button>
-        <button class="btn testimonial-btn ms-2" type="button" data-bs-target="#associationCarousel" data-bs-slide="next">
-          <span class="carousel-control-next-icon-custom" aria-hidden="true">&rsaquo;</span>
-        </button>
+    <div class="association-grid">
+      <div class="association-card">
+        <img src="https://solutionwithsolar.com/public/uploads/associate/UTL-Logo.png" alt="UTL Solar">
       </div>
-
-      <div class="carousel-inner">
-        <!-- Main Logo Row 1 -->
-        <div class="carousel-item active">
-          <div class="row align-items-center justify-content-center g-4">
-            <div class="col-6 col-md-2">
-              <div class="brand-logo-container">
-                <img src="https://seeklogo.com/images/U/utl-solar-logo-E0E9B0E76F-seeklogo.com.png" alt="UTL Solar" class="img-fluid brand-logo">
-              </div>
-            </div>
-            <div class="col-6 col-md-2">
-              <div class="brand-logo-container">
-                <img src="https://companieslogo.com/img/orig/ADANIPOWER.NS-6f3e0981.png?t=1661142517" alt="Adani Power" class="img-fluid brand-logo">
-              </div>
-            </div>
-            <div class="col-6 col-md-2">
-              <div class="brand-logo-container">
-                <img src="https://www.jakson.com/images/jakson-logo.png" alt="Jakson" class="img-fluid brand-logo">
-              </div>
-            </div>
-            <div class="col-6 col-md-2">
-              <div class="brand-logo-container">
-                <img src="https://seeklogo.com/images/E/exide-logo-2D4A1B2E3D-seeklogo.com.png" alt="Exide" class="img-fluid brand-logo">
-              </div>
-            </div>
-            <div class="col-6 col-md-2">
-              <div class="brand-logo-container">
-                <img src="https://companieslogo.com/img/orig/LUMINOUS.NS-0c33a92b.png?t=1648074697" alt="Luminous" class="img-fluid brand-logo">
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- Logo Row 2 (Duplicate for sliding effect or other partners) -->
-        <div class="carousel-item">
-          <div class="row align-items-center justify-content-center g-4">
-            <div class="col-6 col-md-2">
-              <div class="brand-logo-container">
-                <img src="https://seeklogo.com/images/L/luminous-logo-B3F3A3A9D1-seeklogo.com.png" alt="Luminous" class="img-fluid brand-logo">
-              </div>
-            </div>
-            <div class="col-6 col-md-2">
-              <div class="brand-logo-container">
-                 <img src="https://companieslogo.com/img/orig/ADANIPOWER.NS-6f3e0981.png?t=1661142517" alt="Adani Power" class="img-fluid brand-logo">
-              </div>
-            </div>
-             <div class="col-6 col-md-2">
-              <div class="brand-logo-container">
-                <img src="https://seeklogo.com/images/U/utl-solar-logo-E0E9B0E76F-seeklogo.com.png" alt="UTL Solar" class="img-fluid brand-logo">
-              </div>
-            </div>
-             <div class="col-6 col-md-2">
-              <div class="brand-logo-container">
-                <img src="https://seeklogo.com/images/E/exide-logo-2D4A1B2E3D-seeklogo.com.png" alt="Exide" class="img-fluid brand-logo">
-              </div>
-            </div>
-            <div class="col-6 col-md-2">
-              <div class="brand-logo-container">
-                <img src="https://www.jakson.com/images/jakson-logo.png" alt="Jakson" class="img-fluid brand-logo">
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="association-card">
+        <img src="https://solutionwithsolar.com/public/uploads/associate/adani-logo.png" alt="Adani Power">
+      </div>
+      <div class="association-card">
+        <img src="https://solutionwithsolar.com/public/uploads/associate/jakson.png" alt="Jakson">
+      </div>
+      <div class="association-card">
+        <img src="https://solutionwithsolar.com/public/uploads/associate/logo-exide.png" alt="Exide">
+      </div>
+      <div class="association-card">
+        <img src="https://solutionwithsolar.com/public/uploads/associate/LuminousLogoBlue.png" alt="Luminous">
+      </div>
+      <div class="association-card">
+        <img src="https://solutionwithsolar.com/public/uploads/associate/Waaree-logo_vufgvr.png" alt="Waaree">
       </div>
     </div>
   </div>
